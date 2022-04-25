b6e12be9b1897e85408568b3e8879e221de5f576
Tetra <me@tetra.cool>
2022-04-25 14:33:39 -0400

    convert cmd class to object

--- src/tools/cmds.ts
+++ src/tools/cmds.ts
@@ -3,15 +3,17 @@ import os from "os";
 import * as exec from "@actions/exec";
 
 // Aliases for common commands
-export default class Cmd {
-  static async extract(src: string, dest: string): Promise<void> {
+const cmd = {
+  async extract(src: string, dest: string): Promise<void> {
     await exec.exec(`unzip "${src}" -d "${dest}"`);
-  }
-  static async wget(url: string, path: string): Promise<void> {
+  },
+  async wget(url: string, path: string): Promise<void> {
     await exec.exec(
       `${
         os.platform() === "win32" ? "C:\\msys64\\usr\\bin\\wget.exe" : "wget"
       } -q -P "${path}" "${url}"`
     );
   }
-}
+};
+
+export default cmd;
