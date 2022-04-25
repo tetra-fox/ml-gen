e25fd725fa33e3825f865d487a6f44950fe8c75a
Tetra <me@tetra.cool>
2022-04-25 13:52:24 -0400

    define input type

--- src/inputs.ts
+++ src/inputs.ts
@@ -3,33 +3,41 @@ import path from "path";
 
 import * as core from "@actions/core";
 
+// i'll use this later
+interface Input {
+  name: string;
+  value: string;
+  required: boolean;
+}
+
 export default class Inputs {
-  static readonly game = {
+  // We need to specify the name because once minified, input.name is lost
+  static readonly game: Input = {
     name: "game",
     value: core.getInput("game"),
     required: true
   };
-  static readonly gamePath = {
+  static readonly gamePath: Input = {
     name: "game_path",
     value: path.normalize(core.getInput("game_path")),
     required: true
   };
-  static readonly gameExe = {
+  static readonly gameExe: Input = {
     name: "game_executable",
     value: core.getInput("game_executable").replace(/\.exe|\.app$/, ""), // Remove extension
     required: true
   };
-  static readonly unityVersion = {
+  static readonly unityVersion: Input = {
     name: "unity_version",
     value: core.getInput("unity_version"),
     required: false
   };
-  static readonly tmpPath = {
+  static readonly tmpPath: Input = {
     name: "work_path",
     value: path.normalize(core.getInput("work_path")),
     required: false
   };
-  static readonly outPath = {
+  static readonly outPath: Input = {
     name: "output_path",
     value: path.normalize(
       core.getInput("output_path") ||
@@ -37,7 +45,7 @@ export default class Inputs {
     ),
     required: false
   };
-  static readonly mlVersion = {
+  static readonly mlVersion: Input = {
     name: "ml_version",
     value: core.getInput("ml_version"),
     required: false
